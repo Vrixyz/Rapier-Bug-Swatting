4b949e65b6722eb6c5d0910fc69b583d8229b2d5
Thierry Berger <contact@thierryberger.com>
2024-05-17 14:22:54 +0200

    even smaller reproduction

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -21,32 +21,19 @@ fn setup_graphics(mut commands: Commands) {
 }
 
 fn setup_physics(mut commands: Commands) {
-    /* Create the ground. */
-    commands
-        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
-
-    /* Create the bouncing ball. */
     commands
         .spawn(RigidBody::Dynamic)
-        //.insert(Collider::ball(0.5))
-        //.insert(Restitution::coefficient(0.7))
         .insert(TransformBundle::from(Transform {
             translation: Vec3::new(0.13110116, 0.0, 1.3211011),
             rotation: Quat::from_xyzw(0.0, -0.33848378, 0.0, 0.94097227),
             scale: Vec3::new(0.9665972, 0.9665972, 0.9665972),
-        }))
-        .insert(Sleeping {
-            normalized_linear_threshold: 0.01,
-            angular_threshold: 0.01,
-            sleeping: false,
-        });
+        }));
 }
 
 fn print_position(positions: Query<&Transform, With<RigidBody>>) {
     for transform in positions.iter() {
         println!(
-            "Ball position: {} ; rotation: {}",
+            "Body position: {} ; rotation: {}",
             transform.translation, transform.rotation
         );
     }
